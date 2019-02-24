1672d6ec932fb0abb566f267908031f06feeb395
victorybringer <843517609@qq.com>
2019-02-24 14:58:09 +0800

    2019-02-24

---
+++ slice/server.js
@@ -0,0 +1,161 @@
+﻿const express = require('express')
+const app = express()
+
+const exec = require('child_process').exec; 
+ 
+const fs = require('fs')
+
+
+
+
+app.post('/slice', function (req, res) {
+    var code = [];
+
+
+    req.on('data', function (data) {
+
+       code=data.toString()
+
+        fs.writeFile('slice/data/testfile.c', JSON.parse(code).code,  function(err) {
+
+
+            var direction=JSON.parse(code).direction
+
+            var method=JSON.parse(code).method
+            var parallel=JSON.parse(code).parallel
+
+            var image=JSON.parse(code).image
+            var time=JSON.parse(code).time
+
+
+
+
+            exec("llvm-slicing slice/data/testfile.c -d "+direction+" -m  "+method+" -p  "+parallel+" -t  "+time, function(err,stdout,stderr) {
+
+               res.send(stdout+stderr)
+
+
+
+
+
+            })
+
+
+        });
+
+    });
+
+})
+
+
+
+
+
+        
+   
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+	
+ 
+
+
+
+	
+		
+		
+	
+	
+
+
+
+
+
+
+	
+		
+		
+	
+	
+
+
+
+app.post('/image', function (req, res) {
+
+
+  var code = [];
+    
+  
+  req.on('data', function (data) {
+       
+       code.push(data);
+        
+    });
+    
+
+
+
+
+
+fs.writeFile('/usr/local/src/testfile.c', code,  function(err) {});
+
+exec("cd /usr/local/src/ && llvm-slicing testfile.c -g Sdg ", function(err,stdout,stderr){
+
+
+exec("cp /usr/local/src/testfile_SDG.png   /opt/slice/slice/data/", function(err,stdout,stderr){
+
+
+
+ res.end()
+
+})
+
+
+})
+
+})
+	
+
+
+	
+
+
+
+
+app.use(express.static('slice'))
+app.listen(8000, function() {
+
+})
+       
+      
+   
+       
+
+    
+
+
+
+    
+
+
+
+
+
+
+
+
+
---
+++ slice/slice/index.js
@@ -0,0 +1,132 @@
+﻿var editor = ace.edit("editor");
+var Range = ace.require('ace/range').Range;
+var markers=[]
+editor.session.setMode("ace/mode/c_cpp");
+
+
+var app = angular.module('app', []);
+
+app.controller('myCtrl', function($http,$scope) {
+
+    $scope.navstyle = [["", "切片方向"], ["", "切片方法"], ["", "并行设置"], ["", "图像类型"], ["", "时间设置"]]
+
+    $scope.icon = ["glyphicon glyphicon-sort","glyphicon glyphicon-th-large","glyphicon glyphicon-align-justify","glyphicon glyphicon-picture","glyphicon glyphicon-time"]
+    $scope.radiocontent = [["Bwd", "Fwd", "Both"], [" Symbolic", "Weiser", "SDG", "IFDS"], ["False", "True"], ["Sdg", "Cg", "Cdg", "Cfg", "Icfg", "Pdt", "Dt"]]
+    $scope.radiocheck = [[true, false, false], [true, false, false, false], [true, false], [true, false, false, false, false, false, false]]
+
+    $scope.radio = function (e) {
+
+    var a=e.substring(0,1)
+        var b=e.substring(1,2)
+
+
+        for(var i=0;i< $scope.radiocheck[a].length;++i){
+            $scope.radiocheck[a][i]=false
+        }
+        $scope.radiocheck[a][b]=true
+    }
+
+    $scope.nav = function (e) {
+
+
+        if ($scope.navstyle[e][0] == "active") {
+            $scope.navstyle[e][0] = ""
+        } else {
+            $scope.navstyle[e][0] = "active"
+        }
+
+
+        for (var i = 0; i < $scope.navstyle.length; ++i) {
+            if (i != e)
+                $scope.navstyle[i][0] = ""
+        }
+
+
+    }
+
+    $scope.change = function () {
+        var reader = new FileReader();//这里是核心！！！读取操作就是由它完成的。
+        reader.readAsText(document.getElementById("file").files[0]);//读取文件的内容
+
+
+        reader.onload = function () {
+
+
+            editor.setValue(this.result.trim(), -1)
+
+
+            $scope.$apply()
+
+        };
+
+
+    }
+
+    $scope.file = function(){
+
+
+
+        document.getElementById("file").click();
+
+    };
+
+
+    $scope.net= function(){
+var choice=[]
+for(var a=0;a<=3;++a){
+    for(var b=0;b<=$scope.radiocheck[a].length;++b){
+if ($scope.radiocheck[a][b]==true){
+    choice.push($scope.radiocontent[a][b])
+
+}
+
+    }
+
+}
+
+        $http({
+
+            method:'post',
+
+            url:'slice',
+
+            data:{
+              code:  editor.getValue(),
+
+                direction:choice[0],
+                method:choice[1],
+                parallel:choice[2],
+                image:choice[3],
+
+
+                time:$scope.time,
+
+
+
+            } ,
+
+
+        }).success(function(req){
+
+            $scope.result=req.trim()
+
+
+        })
+
+
+
+    };
+
+
+
+$scope.time="1800"
+
+
+})
+
+
+
+
+
+
+
